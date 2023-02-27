f2ac070e3413b92bcf78915ceb7e4476a29b8f0d
Ali Bailoun <bailoun.ali909@outlook.com>
2023-02-26 20:33:33 -0500

    Deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -8,3 +8,7 @@ MechaCar <- read.csv("R_Analysis/MechaCar_Statistical_Analysis/MechaCar_mpg.csv"
 
 MechaLM <- lm(MechaCar, data = MechaCar) #create linear model
 summary(MechaLM)
+
+suspension_table <- read.csv("R_Analysis/MechaCar_Statistical_Analysis/Suspension_Coil.csv", check.names = F, stringsAsFactors = F) #import Suspension Coil Dataset
+total_summary <- suspension_table %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),Var_PSI= var(PSI),SD_PSI= sd(PSI)) #create summary table
+lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),Var_PSI= var(PSI),SD_PSI= sd(PSI)) #create summary table based on Manufacturing lot
