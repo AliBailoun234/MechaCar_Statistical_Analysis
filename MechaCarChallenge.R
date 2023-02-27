76139a272d75a86c5bbedc6dd42247b8949890eb
Ali Bailoun <bailoun.ali909@outlook.com>
2023-02-26 21:43:37 -0500

    Deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,6 +9,13 @@ MechaCar <- read.csv("R_Analysis/MechaCar_Statistical_Analysis/MechaCar_mpg.csv"
 MechaLM <- lm(MechaCar, data = MechaCar) #create linear model
 summary(MechaLM)
 
+#Deliverable 2
 suspension_table <- read.csv("R_Analysis/MechaCar_Statistical_Analysis/Suspension_Coil.csv", check.names = F, stringsAsFactors = F) #import Suspension Coil Dataset
 total_summary <- suspension_table %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),Var_PSI= var(PSI),SD_PSI= sd(PSI)) #create summary table
 lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),Var_PSI= var(PSI),SD_PSI= sd(PSI)) #create summary table based on Manufacturing lot
+
+#Deliverable 3
+t.test(suspension_table$PSI,mu=1500)
+t.test(subset(suspension_table,Manufacturing_Lot=="Lot1")$PSI,mu = 1500)
+t.test(subset(suspension_table,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)
+t.test(subset(suspension_table,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
--- README.md
+++ README.md
@@ -6,6 +6,12 @@
 ## Summary Statistics on Suspension Coils
 
 The mean and median of each lot (lot1, lot2, lot3) are very similar and in some instances the same. This would suggest that graph would be normally distributed.
+
+![Total Summary](totalSummary.PNG)
+
 Based on total summary, the design specifications for the MechaCar suspension coils do meet standards, in which it does not exceed 100 pounds per square inch.
+
+![Lot Summary](LotSummary.PNG)
+
 But based on each lot summary, lot 1 and lot 2 have less than 100 pounds as variance (0.98 and 7.47 respectively). While lot 3 would not meet design specification with a variance equal to 170.
 
